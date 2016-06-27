3a7e5787e48f3ab54cff70a74821d2c2a97cca0e
inozemcev (windows) <= officefish>
2016-06-28 02:52:16 +0300

    add typescript compile task and compile options in tsconfig.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 
 const gulp = require('gulp');
 const tslint = require('gulp-tslint');
+const tsc = require('gulp-typescript');
+
+var source_files_pattern = "source/ts/**/**.ts"
+var source_td_files_pattern = "source/interfaces/interfaces.d.ts"
+var test_files_pattern = "test/ts/**/**.test.ts"
 
 //******************************************************************************
 //* WELCOME
@@ -12,15 +17,43 @@ gulp.task('welcome', function () {
 })
 
 //******************************************************************************
+//* TS COMPILATION
+//******************************************************************************
+
+/*
+* полный список опций компилятора: 
+* https://www.typescriptlang.org/docs/handbook/compiler-options.html
+*
+* пояснения к текущей компиляции:
+* removeComments - удаляем все комментарии
+* noImplicitAny - не ругаемся на any тип переменных
+* target - спецификация javascript (ES3, ES5, ...)
+* module - модульная обертка для соответсвующего стандарта,
+* в данном случае commonjs что позволяет использовать итоговые js файлы
+* в теге <script> 
+* declaration - автоматическое создание файлов деклораций (интрерфейсов)
+*/
+
+var tsProject = tsc.createProject('tsconfig.json');
+gulp.task("scripts", function() {
+    return gulp.src([
+            source_files_pattern,
+            source_td_files_pattern
+        ])
+        .pipe(tsc(tsProject))
+        .js.pipe(gulp.dest("source/"));
+});
+
+
+//******************************************************************************
 //* LINT
 //******************************************************************************
 
 gulp.task('lint', function() {
-    var source_files_pattern = "./source/ts/**/**.ts"
-    var test_files_pattern = "./test/ts/**/**.test.ts"
+   
 
-    return gulp.src([source_files_pattern,
-                     test_files_pattern
+    return gulp.src(["./" + source_files_pattern,
+                     "./" + test_files_pattern
     ])
     .pipe(tslint())
     .pipe(tslint.report('verbose'));
