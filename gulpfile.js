e98de838e32c1681703a915147fe38b77899fdc5
inozemcev (windows) <= officefish>
2016-06-28 01:41:24 +0300

    add tslint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
 "use strict";
 
 const gulp = require('gulp');
+const tslint = require('gulp-tslint')
+
+//******************************************************************************
+//* WELCOME
+//******************************************************************************
 
 gulp.task('welcome', function () {
     console.log('Hello World!');
+})
+
+//******************************************************************************
+//* LINT
+//******************************************************************************
+
+gulp.task('lint', function(){
+    var source_files_pattern = "./source/ts/**/**.ts"
+    var test_files_pattern = "./test/ts/**/**.test.ts"
+
+    return gulp.src([, source_files_pattern,
+                     test_files_pattern
+    ])
+    .pipe(tslint())
+    .pipe(tslint.report('verbose'));
 })
