7dabe3996d95ac92158d8120dbc0963bed0f8875
inozemcev (windows) <= officefish>
2016-06-28 20:36:20 +0300

    Advanced lint, install gulp modules, add gulp.config.js for project

--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,86 @@
-"use strict";
+/*jshint esversion: 6 */
+(function () {
+   'use strict';
+   // this function is strict...
+}());
+
+const config = require('./gulp.config')();
 
 const gulp = require('gulp');
-const tslint = require('gulp-tslint');
-const tsc = require('gulp-typescript');
+const $ = require('gulp-load-plugins')({ lazy: true });
+const tslintStylish = require('tslint-stylish');
+const args = require('yargs').argv;
 
-const browserify = require('browserify');
-const transform = require('vinyl-transform');
-const uglify = require('gulp-uglify')
-const sourcemaps = require('gulp-sourcemaps')
 
-var browserified = transform(function(filename) {
-var b = browserify({ entries: filename, debug: true });
-return b.bundle();
+
+
+/**
+ * List the available gulp tasks
+ */
+gulp.task('help', $.taskListing.withFilters(/:/));
+gulp.task('default', ['help']);
+
+/**
+ * Lint
+ */
+
+/**
+ * vet typescript code
+ * @return {Stream}
+ */
+gulp.task('vet:typescript', function () {
+
+    log('Analyzing typescript code with TSLint');
+
+    return gulp
+        .src(config.ts.files)
+        .pipe($.tslint())
+        .pipe($.tslint.report(tslintStylish, {
+            emitError: false,
+            sort: true,
+            bell: false
+        }));
 });
 
+/**
+ * vet es5 code
+ * --verbose
+ * @return {Stream}
+ */
+gulp.task('vet:es5', function(){
 
-var source_files_pattern = "source/ts/**/**.ts"
-var source_td_files_pattern = "source/interfaces/interfaces.d.ts"
-var test_files_pattern = "test/ts/**/**.test.ts"
+    log('Analyzing ES5 code with JSHint');
 
-//******************************************************************************
-//* WELCOME
-//******************************************************************************
-
-gulp.task('welcome', function () {
-    console.log('Hello World!');
-})
-
-//******************************************************************************
-//* TS COMPILATION
-//******************************************************************************
-
-/*
-* полный список опций компилятора: 
-* https://www.typescriptlang.org/docs/handbook/compiler-options.html
-*
-* пояснения к текущей компиляции:
-* removeComments - удаляем все комментарии
-* noImplicitAny - не ругаемся на any тип переменных
-* target - спецификация javascript (ES3, ES5, ...)
-* module - модульная обертка для соответсвующего стандарта,
-* в данном случае commonjs что позволяет использовать итоговые js файлы
-* в теге <script> 
-* declaration - автоматическое создание файлов деклораций (интрерфейсов)
-*/
-
-var tsProject = tsc.createProject('tsconfig.json');
-gulp.task("scripts", function() {
-    return gulp.src([
-            source_files_pattern,
-            source_td_files_pattern
-        ])
-        .pipe(tsc(tsProject))
-        .js.pipe(gulp.dest("source/"));
+    return gulp
+        .src(config.js.root)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
-//******************************************************************************
-//* TS TESTS COMPILATION
-//******************************************************************************
-
-var tsTestProject = tsc.createProject('tsconfig.json');
-gulp.task('tsc-tests', function() {
-    return gulp.src('./test/**/**.test.ts')
-        .pipe(tsc(tsTestProject ))
-        .js.pipe(gulp.dest('./temp/test/'));
+/**
+ * Vet both ES5 and TypeScript
+ * @return {Stream}
+ */
+gulp.task('scripts-vet', ['vet:es5', 'vet:typescript'], function () {
 });
 
+/**
+ * Log a message or series of messages using chalk's blue color.
+ * Can pass in a string, object or array.
+ */
+function log(msg) {
 
-//******************************************************************************
-//* LINT
-//******************************************************************************
-
-gulp.task('lint', function() {
-   
-
-    return gulp.src(["./" + source_files_pattern,
-                     "./" + test_files_pattern
-    ])
-    .pipe(tslint())
-    .pipe(tslint.report('verbose'));
-})
-
-
-//******************************************************************************
-//* COMPLECSE BUILD
-//******************************************************************************
-
-gulp.task('build',['scripts','tsc-tests', 'lint']);
-
-
-//******************************************************************************
-//* DEFAULT
-//******************************************************************************
-
-gulp.task('default',['lint','scripts']);
-
-
-//******************************************************************************
-//* BUNDLE
-//******************************************************************************
-
-var tempProject = tsc.createProject('tsconfig.json');
-gulp.task("temp-scripts", function() {
-    return gulp.src([
-            ".temp/source/js/**/**.ts",
-            ".temp/source/js/**/**.d.ts"
-        ])
-        .pipe(tsc(tempProject))
-        .js.pipe(gulp.dest(".temp/source/js/"));
-});
-
-gulp.task('temp-bundle', function () {
-return gulp.src('.temp/source/js/main.js')
-.pipe(browserified)
-.pipe(sourcemaps.init({ loadMaps: true }))
-.pipe(uglify())
-.pipe(sourcemaps.write('./'))
-.pipe(gulp.dest('./dist/source/js/'));
-});
-
-gulp.task('temp',['temp-scripts','temp-bundle']);
-
+    if (typeof (msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                 console.log(msg[item]);
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        console.log(msg);
+        $.util.log($.util.colors.blue(msg));
+    }
+}
